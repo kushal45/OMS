933eddc70784f11ae73a9371a146f79553b2c416
I549353 <kushal.bhattacharya@sap.com>
2024-11-11 17:03:30 +0530

    address lib created for consumption accross multiple apps

---
+++ libs/address/src/entity/address.entity.ts
@@ -0,0 +1,22 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Address {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  street: string;
+
+  @Column()
+  city: string;
+
+  @Column()
+  state: string;
+
+  @Column()
+  country: string;
+
+  @Column()
+  postalCode: string;
+}
