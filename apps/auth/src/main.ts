345cfb0a07f837c55852e33dafe1e3d19e2dae80
I549353 <kushal.bhattacharya@sap.com>
2024-11-23 23:07:47 +0530

    using reusable swagger lib module to add Swagger documentation in auth endpoint

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -3,18 +3,12 @@ import { AuthModule } from './auth.module';
 import { ConfigService } from '@nestjs/config';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import { setupSwagger } from '@lib/swagger/swagger.controller';
 
 async function bootstrap() {
   const app = await NestFactory.create(AuthModule);
-  const config = new DocumentBuilder()
-    .setTitle('Auth Microservice')
-    .setDescription('Authentication API documentation')
-    .setVersion('1.0')
-    .addBearerAuth() // Optional: for JWT or Bearer token authentication
-    .build();
   app.useGlobalPipes(new ValidationPipe());
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('auth/docs', app, document); // Custom route for Swagger UI
+  setupSwagger(app, "auth/docs");
   const configService = app.get(ConfigService);
   console.log(configService);
   const port = configService.get<number>('PORT');
